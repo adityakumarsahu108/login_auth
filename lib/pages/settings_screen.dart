5d431398a6e11624b00db007d27f4fbb61d1b926
adityakumarsahu108 <adityakumarsahu108@gmail.com>
2024-03-07 09:30:44 +0530

    signout

--- lib/pages/settings_screen.dart
+++ lib/pages/settings_screen.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SettingsScreen extends StatefulWidget {
-  const SettingsScreen({super.key});
+  const SettingsScreen({Key? key}) : super(key: key);
 
   @override
   State<SettingsScreen> createState() => _SettingsScreenState();
 }
 
 class _SettingsScreenState extends State<SettingsScreen> {
+  // Function to handle sign-out
+  Future<void> signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+
+      FirebaseAuth.instance.signOut();
+      Navigator.pop(context);
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,7 +38,21 @@ class _SettingsScreenState extends State<SettingsScreen> {
         ),
       ),
       body: SafeArea(
-        child: Text('settings screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: MaterialButton(
+                onPressed: signOut,
+                color: Colors.deepPurple[500],
+                child: const Text(
+                  'Sign Out',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
